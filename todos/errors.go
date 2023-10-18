4d8b9de506683bee2e0b6d2d18daeba744a2d449
Diego Quintana <dquintana@utb.edu.co>
2023-10-18 18:16:43 -0500

    Agrege algunos errores

--- todos/errors.go
+++ todos/errors.go
@@ -3,3 +3,15 @@ package todos
 var ERRORS = map[string]string{
 	"DONT_EXIST_TODO": "Warning: Don't exist.",
 }
+
+type DONT_EXIST_TODO struct{}
+
+func (e *DONT_EXIST_TODO) Error() string {
+	return "El ID proporcionado, no existe."
+}
+
+type NEW_TODO_FORMAT_ERROR struct{}
+
+func (e *NEW_TODO_FORMAT_ERROR) Error() string {
+	return "Nombre o descripción invalidos."
+}
